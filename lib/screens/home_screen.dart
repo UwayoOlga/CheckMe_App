af41a18c9b25b4a3c9678c2a9bc8ab10e4d03ea6
UwayoOlga <uwayoolga@gmail.com>
2025-05-08 19:00:07 +0200

    Added the search bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   List<Todo> _todos = [];
   TodoFilter _currentFilter = TodoFilter.all;
+  String _searchQuery = '';
 
   void _addTodo(Todo todo) {
     setState(() {
@@ -39,21 +40,26 @@ class _HomeScreenState extends State<HomeScreen> {
           label: 'UNDO',
           textColor: Colors.pink[200],
           onPressed: () {
-           },
+          },
         ),
       ),
     );
   }
 
   List<Todo> get _filteredTodos {
+    var filteredList = _todos.where((todo) {
+      return todo.title.toLowerCase().contains(_searchQuery.toLowerCase()) ||
+          (todo.description != null && todo.description!.toLowerCase().contains(_searchQuery.toLowerCase()));
+    }).toList();
+
     switch (_currentFilter) {
       case TodoFilter.completed:
-        return _todos.where((todo) => todo.isDone).toList();
+        return filteredList.where((todo) => todo.isDone).toList();
       case TodoFilter.pending:
-        return _todos.where((todo) => !todo.isDone).toList();
+        return filteredList.where((todo) => !todo.isDone).toList();
       case TodoFilter.all:
       default:
-        return _todos;
+        return filteredList;
     }
   }
 
@@ -118,76 +124,97 @@ class _HomeScreenState extends State<HomeScreen> {
           SizedBox(width: 16),
         ],
       ),
-      body: _todos.isEmpty
-          ? Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.check_circle_outline, size: 64, color: Colors.pink[200]),
-            SizedBox(height: 16),
-            Text('No todos yet!', style: TextStyle(fontSize: 18)),
-            Text('Tap the + button to add one', style: TextStyle(color: Colors.grey)),
-          ],
-        ),
-      )
-          : ListView.builder(
-        itemCount: _filteredTodos.length,
-        itemBuilder: (context, index) {
-          final todo = _filteredTodos[index];
-          return Dismissible(
-            key: ValueKey(todo.createdAt),
-            background: Container(color: Colors.red),
-            onDismissed: (_) => _deleteTodo(_todos.indexOf(todo)),
-            child: Card(
-              margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              child: ListTile(
-                title: Text(
-                  todo.title,
-                  style: TextStyle(
-                    decoration: todo.isDone ? TextDecoration.lineThrough : null,
-                    color: todo.isDone ? Colors.grey : Theme.of(context).textTheme.titleMedium?.color,
-                  ),
-                ),
-                leading: Checkbox(
-                  value: todo.isDone,
-                  onChanged: (_) => _toggleTodoStatus(_todos.indexOf(todo)),
-                  activeColor: Colors.pink,
-                ),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    if (todo.description != null && todo.description!.isNotEmpty)
-                      Text(todo.description!),
-                    SizedBox(height: 4),
-                    Text(
-                      'Created: ${todo.createdAt.toString().substring(0, 10)}',
-                      style: TextStyle(
-                        fontSize: 12,
-                        color: Colors.grey,
-                      ),
-                    ),
-                    if (_isOverdue(todo))
-                      Text(
-                        'Overdue',
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Search',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _searchQuery = value;
+                });
+              },
+            ),
+          ),
+          _todos.isEmpty
+              ? Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.check_circle_outline, size: 64, color: Colors.pink[200]),
+                SizedBox(height: 16),
+                Text('No todos yet!', style: TextStyle(fontSize: 18)),
+                Text('Tap the + button to add one', style: TextStyle(color: Colors.grey)),
+              ],
+            ),
+          )
+              : Expanded(
+            child: ListView.builder(
+              itemCount: _filteredTodos.length,
+              itemBuilder: (context, index) {
+                final todo = _filteredTodos[index];
+                return Dismissible(
+                  key: ValueKey(todo.createdAt),
+                  background: Container(color: Colors.red),
+                  onDismissed: (_) => _deleteTodo(_todos.indexOf(todo)),
+                  child: Card(
+                    margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    child: ListTile(
+                      title: Text(
+                        todo.title,
                         style: TextStyle(
-                          color: Colors.red,
-                          fontWeight: FontWeight.bold,
+                          decoration: todo.isDone ? TextDecoration.lineThrough : null,
+                          color: todo.isDone ? Colors.grey : Theme.of(context).textTheme.titleMedium?.color,
                         ),
                       ),
-                  ],
-                ),
-                onTap: () {
-                   Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TaskDetailScreen(todo: todo),
+                      leading: Checkbox(
+                        value: todo.isDone,
+                        onChanged: (_) => _toggleTodoStatus(_todos.indexOf(todo)),
+                        activeColor: Colors.pink,
+                      ),
+                      subtitle: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          if (todo.description != null && todo.description!.isNotEmpty)
+                            Text(todo.description!),
+                          SizedBox(height: 4),
+                          Text(
+                            'Created: ${todo.createdAt.toString().substring(0, 10)}',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey,
+                            ),
+                          ),
+                          if (_isOverdue(todo))
+                            Text(
+                              'Overdue',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                        ],
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => TaskDetailScreen(todo: todo),
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.pink,
